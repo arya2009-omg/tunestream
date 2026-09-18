bb51ddc3245727552d7af8e4055352d1a011b410
arya2009-omg <singharya200902@gmail.com>
2026-09-18 23:06:53 +0530

    chore: bump PWA cache for latest UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="tunestream-v4";
+const CACHE_NAME="tunestream-v5";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
 
 self.addEventListener("install",event=>{
