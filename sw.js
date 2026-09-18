58e6a44321b5585563da6d8d4db09134ab88657c
arya2009-omg <singharya200902@gmail.com>
2026-09-18 22:26:52 +0530

    Bump PWA cache to ensure latest TuneStream UI loads after refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="tunestream-v3";
+const CACHE_NAME="tunestream-v4";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
 
 self.addEventListener("install",event=>{
