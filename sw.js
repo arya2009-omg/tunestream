96ba9457009cda5b992531cb2e3bc6283846eba5
arya2009-omg <singharya200902@gmail.com>
2026-09-18 23:13:27 +0530

    Refresh PWA cache for stable viewport layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="tunestream-v7";
+const CACHE_NAME="tunestream-v8";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
 
 self.addEventListener("install",event=>{
