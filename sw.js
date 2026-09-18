d04a9ad73ff3854d38566dc33294d883b8bf5801
arya2009-omg <singharya200902@gmail.com>
2026-09-18 22:19:31 +0530

    Finalize PWA auto-update service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,43 @@
-const CACHE_NAME="tunestream-v1789749676190";
+const CACHE_NAME="tunestream-v3";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
+
 self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(APP_SHELL))
+      .then(()=>self.skipWaiting())
+  );
 });
+
 self.addEventListener("activate",event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith("tunestream-v")&&key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys
+          .filter(key=>key.startsWith("tunestream-v")&&key!==CACHE_NAME)
+          .map(key=>caches.delete(key))
+      ))
+      .then(()=>self.clients.claim())
+  );
 });
+
+self.addEventListener("message",event=>{
+  if(event.data?.action==="skipWaiting") self.skipWaiting();
+});
+
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET") return;
   const url=new URL(event.request.url);
-  if(url.origin===location.origin){
-    event.respondWith(
-      fetch(event.request).then(response=>{
+  if(url.origin!==location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
         if(response.ok){
           const copy=response.clone();
           caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{});
         }
         return response;
-      }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html")))
-    );
-  }
+      })
+      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html")))
+  );
 });
