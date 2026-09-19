5c6dc8772b3a6230070e367c65b6730c002863df
arya2009-omg <singharya200902@gmail.com>
2026-09-19 10:54:19 +0530

    fix: point mobile API fallback to Render server

--- spotify-mobile/lib/api.ts
+++ spotify-mobile/lib/api.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-export const SERVER_URL=process.env.EXPO_PUBLIC_API_URL||"http://192.168.1.5:5000";
-const api=axios.create({baseURL:SERVER_URL+"/api",timeout:15000});
-api.interceptors.request.use(async config=>{
-  const token=await AsyncStorage.getItem("token");
-  if(token){config.headers=config.headers||{};config.headers.Authorization="Bearer "+token}
+
+export const SERVER_URL =
+  process.env.EXPO_PUBLIC_API_URL || "https://tunestream-fth1.onrender.com";
+
+const api = axios.create({
+  baseURL: SERVER_URL + "/api",
+  timeout: 15000,
+});
+
+api.interceptors.request.use(async config => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = "Bearer " + token;
+  }
   return config;
 });
+
 export default api;
