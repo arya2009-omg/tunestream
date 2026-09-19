929147c1b3d19870e6da255f5da7724ef3ece9ee
arya2009-omg <singharya200902@gmail.com>
2026-09-19 07:03:00 +0530

    Make mobile API URL configurable for LAN backend

--- spotify-mobile/lib/api.ts
+++ spotify-mobile/lib/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-export const SERVER_URL="http://192.168.1.5:5000";
-const api=axios.create({baseURL:SERVER_URL+"/api",timeout:10000});
-api.interceptors.request.use(async config=>{const token=await AsyncStorage.getItem("token");if(token)config.headers.Authorization="Bearer "+token;return config;});
+export const SERVER_URL=process.env.EXPO_PUBLIC_API_URL||"http://192.168.1.5:5000";
+const api=axios.create({baseURL:SERVER_URL+"/api",timeout:15000});
+api.interceptors.request.use(async config=>{
+  const token=await AsyncStorage.getItem("token");
+  if(token){config.headers=config.headers||{};config.headers.Authorization="Bearer "+token}
+  return config;
+});
 export default api;
